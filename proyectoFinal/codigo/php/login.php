00a5d4b652420e13ae37975f47e2a296b2a93a4c
Alejandro Rodriguez Campelo <alejandrorodriguezcampelo@gmail.com>
2023-05-01 18:51:47 +0200

    paso al server de xamp

--- proyectoFinal/codigo/php/login.php
+++ proyectoFinal/codigo/php/login.php
@@ -4,9 +4,27 @@ $con = new Conexion();
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     echo "esto es get";
+    echo "hola";
+    try {
+        echo"hola";
+        $sql = "SELECT * FROM socios WHERE 1 ";
+        $result = $con->query($sql);
+        //comprovamos que alla resultado
+        if($result->num_rows == 0 ){
+            header("HTTP/1.1 406 Not Acceptable");
+        }else{
+        $usuario = $result->fetch_all(MYSQLI_ASSOC);
+        echo json_encode($usuario);
+        }
+    }
+    catch (mysqli_sql_exception $e) {
+        header("HTTP/1.1 404 Not Found");
+    }
+    exit;
 }
 else{
     echo "esto no es get";
 }
 echo "llego al final";
+echo "<br><h1>nose no funciona</h1>";
 ?>
